36ea9d9c009f78974fec5d2da2f698f177e7c542
DarkeEnergyy <nightkiller7@mail.ru>
2024-10-12 16:07:22 +0300

    возвращение потока, индекс содержимого файла

--- 1  laba.cpp
+++ 1  laba.cpp
@@ -1,187 +1,186 @@
 ﻿#include <iostream>
-#include <vector>
 #include <string>
 #include <fstream>
+#include <typeinfo>
 using namespace std;
 
 struct Puppe {
-    string name = "";
-    float len = -1;
-    float diam = -1;
+    string name;
+    float len=-1;
+    float diam=-1;
     int fix = -1;
 };
 
 struct KS {
-    string name = "";
-    int rooms = -1;
-    int work = -1;
-    float kpd= -1;
+    string name;
+    int rooms=-1;
+    int work=-1;
+    float kpd=-1;
 };
+template <typename T1>
+T1 proverka(T1 min, T1 max) {
+    T1 i;
+    cout << "Enter number in range: "<<min<<"; "<<max << endl;
+    while (true) {
+        cin >> i;
+        if (cin.fail() || i < min || i>max) {
+            cout << "Enter Correct data" << endl;
+            cin.clear();
+            cin.ignore(100, '\n');
+            continue;
+        }
+        return i;
+    }
+}
 
-int operator << (fstream& in, Puppe& puppe) {
-    int fl=1;
+fstream& operator >> (fstream& f, Puppe& p) {
+    getline(f>>ws, p.name);
+    f >> p.len; 
+    f >> p.diam; 
+    f >> p.fix;
+    if (p.name == "") {
+        p = {};
+    }
+    if (f.fail())
+    {
+        cout << "Error data in file" << endl;
+        p = {};
+    }
+    return f;
+}
+
+fstream& operator >> (fstream& f, KS& k) {
+    getline(f>>ws, k.name);
+    if (k.name == "") {
+        k = {};
+        return f;
+    }
+    f >> k.rooms;
+    f >> k.work;
+    f >> k.kpd;
+    if (f.fail())
+    {
+        cout << "Error data in file" << endl;
+        k = {};
+    }
+    return f;
+}
+
+fstream& operator << (fstream& in, Puppe& puppe) {
     if (puppe.name != "") {
-        in << puppe.name << " ";
+        in << puppe.name << '\n';
     }
     else {
-        cout << "Pipe"<<endl<<"No name" << endl;
-        fl = 0;
+        cout << "No Pipe" << endl;
+        return in;
     }
-    if (puppe.diam != -1) {
-        in << puppe.len << " ";
-        fl = 0;
-    }
-    else {
-        cout << "No len" << endl;
-        fl = 0;
-    }
-    if (puppe.diam != -1) {
-        in << puppe.diam << " ";
-    }
-    else {
-        cout << "No diam" << endl;
-        fl = 0;
-    }
-    if (puppe.fix == 1 || puppe.fix == 0) {
-        in << puppe.fix << " ";
-    }
-    else {
-        cout << "No fix" << endl;
-        fl = 0;
-    }
-    return fl;
+    in << puppe.len << '\n';
+    in << puppe.diam << '\n';
+    in << puppe.fix << '\n';
+    return in;
 }
 
-void operator << (fstream& in, KS& ks) {
+fstream& operator << (fstream& in, KS& ks) {
     if (ks.name != "") {
-        in << ks.name << " ";
+        in << ks.name << '\n';
     }
     else {
-        cout << "KS:"<<endl<< "No name" << endl;
+        cout << "No KS" << endl;
+        return in;
     }
-    if (ks.kpd != -1) {
-        in << ks.kpd << " ";
-    }
-    else {
-        cout << "No kpd" << endl;
-    }
-    if (ks.rooms != -1) {
-        in << ks.rooms << " ";
-    }
-    else {
-        cout << "No rooms" << endl;
-    }
-    if (ks.work != -1) {
-        in << ks.work << " ";
-    }
-    else {
-        cout << "No work" << endl;
-    }
+    in << ks.kpd << '\n';
+    in << ks.rooms << '\n';
+    in << ks.work << '\n';
+    return in;
 }
 
-void operator >> (istream& in, Puppe& puppe) {
-    cin.ignore(1000, '\n');
+istream& operator >> (istream& in, Puppe& puppe) {
     cout << "Enter name:" << endl;
-    getline(cin, puppe.name);
+    in.ignore(1000, '\n');
+    getline(in, puppe.name);
     cout << "Enter length:" << endl;
-    in >> puppe.len;
-    if (cin.fail() || puppe.len <= 0) {
-        cout << "\nEnter Correct data" << endl;
-    }
+    puppe.len = proverka(0.01, 10000.0);
     cout << "Enter diametr:" << endl;
-    in >> puppe.diam;
-    if (cin.fail() || puppe.diam <= 0) {
-        cout << "\nEnter Correct data" << endl;
+    puppe.diam = proverka(0.01, 10000.0);
+    cout << "Enter fix:" << endl;
+    puppe.fix = proverka(0, 1);
+    return in;
+}
+
+istream& operator >> (istream& in, KS& ks) {
+    cout << "Enter name:" << endl;
+    in.ignore(1000, '\n');
+    getline(in, ks.name);
+    cout << "Enter rooms:" << endl;
+    ks.rooms = proverka(1, 1000);
+    cout << "Enter work:" << endl;
+    ks.work = proverka(0, 1000);
+    cout << "Enter kpd:" << endl;
+    ks.kpd = proverka(0.0, 100.0);
+    return in;
+}
+
+ostream& operator << (ostream& ou, Puppe& puppe) {
+    if (puppe.len != -1) {
+        ou << "Pipe:\n" << "Name: " << puppe.name << endl;
+        ou << "len: " << puppe.len << endl;
+        ou << "diam: " << puppe.diam << endl;
+        ou << "fix: " << puppe.fix << endl;
+    }
+    return ou;
+}
+
+ostream& operator << (ostream& ou, KS& ks) {
+    if (ks.rooms != -1) {
+        ou << "KS:\n"<<"Name: " << ks.name << endl;
+        ou << "Rooms: " << ks.rooms << endl;
+        ou << "Work: " << ks.work << endl;
+        ou << "Kpd: " << ks.kpd << endl;
+    }
+    return ou;
+}
+
+void setFix(Puppe& chfx) {
+    if (chfx.diam == -1) {
+        cout << "No pipe" << endl;
+        return;
     }
     cout << "Enter fix:" << endl;
-    in >> puppe.fix;
-    if (cin.fail() || puppe.fix != 0 || puppe.fix != 1) {
-        cout << "\nEnter Correct data" << endl;
-    }
+    chfx.fix = proverka(0, 1);
 }
 
-void operator >> (istream& in, KS& ks) {
-    cout << "Enter name:" << endl;
-    if (ks.name != " ") {
-        cin.ignore(1000, '\n');
-        getline(cin, ks.name);
+void setWork(KS& chwk) {
+    if (chwk.rooms == -1) {
+        cout << "No pipe" << endl;
+        return;
     }
-    cout << "Enter rooms:" << endl;
-    in >> ks.rooms;
-    if (cin.fail() || ks.rooms < 0) {
-        cout << "\nEnter Correct data" << endl;
-    }
-    cout << "Enter work:" << endl;
-    in >> ks.work;
-    if (cin.fail() || ks.work < 0) {
-        cout << "\nEnter Correct data" << endl;
-    }
-    cout << "Enter kpd:" << endl;
-    in >> ks.kpd;
-    if (cin.fail() || ks.kpd <0 || ks.kpd >= 100) {
-        cout << "\nEnter Correct data" << endl;
-    }
-}
-
-void operator << (ostream& ou, Puppe& puppe) {
-    if (puppe.name != " ") {
-        cout << "Name: " << puppe.name << endl;
-    }
-    if (puppe.len != -1) {
-        cout << "len: " << puppe.len << endl;;
-    }
-    if (puppe.diam != -1) {
-        cout << "diam: " << puppe.diam << endl;
-    }
-    if (puppe.fix != -1) {
-        cout << "fix: " << puppe.fix << endl;
-    }
-}
-
-void operator << (ostream& ou, KS& ks) {
-    if (ks.name != " ") {
-        cout << "Name: " << ks.name << endl;
-    }
-    if (ks.rooms != -1) {
-        cout << "Rooms: " << ks.rooms << endl;
-    }
-    if (ks.work != -1) {
-        cout << "Work" << ks.work << endl;
-    }
-    if (ks.kpd != -1) {
-        cout << "Kpd: " << ks.kpd << endl;
-    }
+    cout << "Enter fix:" << endl;
+    chwk.work = proverka(0, 1);
 }
 
 void WriteToFile(fstream& f, Puppe& puppe, KS& ks) {
-    if (f << puppe) {
-        f << puppe;
+    string ind = "0";
+    if (puppe.name != "") {
+        ind = "1";
     }
-    else {
-        f << '\n';
+    (ks.name != "")?  ind += " 1": ind += " 0";
+    f << ind<<'\n';
+    if (puppe.name != "") {
+        f << puppe;
+
     }
     f << ks;
     f.close();
 }
 
-void ReadFile(Puppe& p, KS& k) {
-    string outs;
-    fstream file("file1.txt");
-    if (!file.is_open()) {
-        cout << "Error open file";
-    }
-    cout << "Pipe:" << endl;
-    getline(file, outs);
-    if (outs == ""){
-        p.diam = -1; p.fix = -1; p.len = -1; p.name = "";
-    }
-    cout << outs<<endl;
-    cout << "KS:" << endl;
-    getline(file, outs);
-    if (outs == "") {
-        k.kpd = -1; k.rooms = -1; k.work = -1; k.name = "";
-    }
-    cout << outs << endl;
+void ReadFile(Puppe& p, KS& k, fstream& file) {
+    string ind;
+    getline(file >> ws, ind);
+    if (ind[0] == '1') { file >> p; }
+    if (ind[2] == '1') { file >> k; }
+    else { cout << "No data in file\n"; }
+    cout << p;
+    cout << k;
 }
 
 int main()
@@ -189,7 +188,7 @@ int main()
     fstream file("file1.txt");
     if (!file.is_open()) {
         cout << "Error open file";
-        return 0;
+        return 0; 
     }
     Puppe puppe1;
     KS ks1;
@@ -198,41 +197,29 @@ int main()
     while (fl) {
         cout << "\n1. Create Pipe\n2. Create KS\n3. Change fixing status\n4. Change number of working KS\n5. Write to file\n6. Read from file\n7. Read data\n8. Exit" << endl;
         cin >> choice;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Only digit" << endl;
+            continue;
+        }
         switch (choice) {
         case 1:
             cin >> puppe1; break;
         case 2:
             cin >> ks1; break;
         case 3:
-            int fx;
-            cin >> fx;
-            if (cin.fail() || fx < 0) {
-                cout << "\nIncorrect data" << endl;
-            }
-            puppe1.fix = fx;
-            break;
+            setFix(puppe1); break;
         case 4:
-            int wk;
-            cin >> wk;
-            if (cin.fail() || wk < 0) {
-                cout << "\nIncorrect data" << endl;
-            }
-            puppe1.fix = fx;
-            break;
+            setWork(ks1); break;
         case 5:
+            file.seekp(0, ios::beg);
             WriteToFile(file, puppe1, ks1); break;
         case 6:
-            ReadFile(puppe1, ks1); break;
+            ReadFile(puppe1, ks1, file); break;
         case 7:
-            int choice2;
-            cout << "What do you want to see?\n1. Pipe\n2. KS" << endl;
-            cin >> choice2;
-            switch (choice2) {
-            case 1:
-                cout << puppe1; break;
-            case 2:
-                cout << ks1; break;
-            }
+            cout << puppe1;
+            cout << ks1;
             break;
         case 8:
             fl = 0; break;
